6af6b1aa5a9d45fec0575ca27a8cb9125760474c
essencec <45109455+essencec@users.noreply.github.com>
2020-06-12 13:36:38 -0400

    Modify code to allow user to select the amount of comments (#10)
    * Modify code to allow user to select the amount of comments
    
    * Modify ArrayList to read information from the datastore
    
    * Changes according to reviewers comments
    
    * Modify code to check params and throw an error message if input is invalid
    
    * Update status code for invalid input

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -17,6 +17,7 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.gson.Gson;
@@ -26,32 +27,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.List;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-    ArrayList<String> jsonData = new ArrayList<String>();
+    
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {   
+        String numOfCommentsString = request.getParameter("numOfComments");
+        int numOfComments;
+        try {
+            numOfComments  = Integer.parseInt(numOfCommentsString);
+        } catch(NumberFormatException e){  
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setContentType("text/html;");
+        response.getWriter().println("Error: `numOfComments` is not a string");
+        return;
+        }
+     
         Query query = new Query("Comment");
-
         PreparedQuery results = datastore.prepare(query);
-        for (Entity entity : results.asIterable()) {
-            String text = (String) entity.getProperty("text");
+        List<Entity> entity = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(numOfComments));
+        ArrayList<String> jsonData = new ArrayList<String>();
+        for(int i = 0; i < entity.size(); i++) {
+            String temp = (String) entity.get(i).getProperty("text");
+            jsonData.add(temp);
         }
         
-        String json = convertToJson(jsonData);
         response.setContentType("application/json;");
+        String json = convertToJson(jsonData);
         response.getWriter().println(json);
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String text = getParameter(request, "text-input", "");
-        jsonData.add(text);
 
         Entity commentEntity = new Entity("Comment");
         commentEntity.setProperty("text", text);
@@ -61,7 +75,7 @@ public class DataServlet extends HttpServlet {
         response.setContentType("text/html;");
         response.getWriter().println(text);
 
-        response.sendRedirect("/data");
+        response.sendRedirect("/index.html");
     }
 
     private String getParameter(HttpServletRequest request, String name, String defaultValue) {
