a498d77aa5d21cd6bae905e65fb6e68fc70f59fe
Essence Cain <evcain@aggies.ncat.edu>
2020-06-15 16:33:12 +0000

    Implemented pie chart to main page using google charts library

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Hobby');
+  data.addColumn('number', 'Count');
+        data.addRows([
+          ['Reading', 20],
+          ['Baking', 10],
+          ['Listening to Music', 15],
+          ['Watching Movies', 25],
+          ['Spending Time with Friends and Family', 30]
+        ]);
+
+  const options = {
+    'title': 'Hobbies',
+    'width':500,
+    'height':400
+  };
+
+  const chart = new google.visualization.PieChart(
+      document.getElementById('chart-container'));
+  chart.draw(data, options);
+}
+
 function getComments() {
   const val = document.getElementById("comment-number").value;
   fetch('/data?numOfComments='+val).then(response => response.json()).then((jsonData) => {
